e8919def7da8a3ae6f5cf84b688de64eb239d301
x-MarkTheSpot <markyrasonabe2405@gmail.com>
2024-09-23 22:48:30 +0800

    Facts about Kurasoy

---
+++ src/it20/It20.java
@@ -0,0 +1,11 @@
+package it20;
+
+public class It20 {
+    public static void main(String[] args) {
+        String igop[] = {"gwapo kay si "};
+        
+        for (String word : igop) {
+            System.out.print(word + "kurasoy");
+        }
+    }
+}
